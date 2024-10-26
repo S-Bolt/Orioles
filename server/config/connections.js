12d9e818d9105dae324dc4356acd5ba537a6465a
Sam <sambolton.kw@gmail.com>
2024-10-25 23:12:55 -0400

    deployment fixes

--- server/config/connections.js
+++ server/config/connections.js
@@ -7,10 +7,26 @@ const Sequelize = require("sequelize");
     process.env.PG_USER,
     process.env.PG_PASSWORD,
     {
-      host: 'localhost',
-      port: process.env.PG_PORT || 5432, 
+      host: process.env.PG_HOST,
+      port: process.env.PG_PORT,
       dialect: 'postgres',
-    },
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, 
+        },
+      },
+      logging: false, 
+    }
   );
+  // Test the connection
+sequelize.authenticate()
+.then(() => {
+  console.log('Connection has been established successfully.');
+})
+.catch((error) => {
+  console.error('Unable to connect to the database:', error);
+});
+  
 
 module.exports = sequelize;
