359a318cb841496fb8ffff1e935a11c2477e1d1d
Sam <sambolton.kw@gmail.com>
2024-10-26 21:07:41 -0400

    fix for images

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,5 @@
 export const API_BASE_URL = 
 import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api';
+
+export const IMAGE_BASE_URL = 
+import.meta.env.VITE_IMAGE_BASE_URL || 'http://localhost:3000/';
