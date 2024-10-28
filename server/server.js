47cc81130860361c6dc59bfb459067335caa3d5c
Sam <sambolton.kw@gmail.com>
2024-10-27 20:59:03 -0400

    change

--- server/server.js
+++ server/server.js
@@ -11,6 +11,20 @@ const { User, Comments, BlogPosts } = require('./models');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const { exec } = require('child_process');
+
+exec('npm run migrate', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Migration error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`Migration stderr: ${stderr}`);
+    return;
+  }
+  console.log(`Migration stdout: ${stdout}`);
+});
+
 // Log Sequelize associations for debugging
 console.log('User Associations:', User.associations);
 console.log('Comment Associations:', Comments.associations);
@@ -35,19 +49,7 @@ sequelize.authenticate()
     
   });
 
-  const { exec } = require('child_process');
-
-exec('npm run migrate', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Migration error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.error(`Migration stderr: ${stderr}`);
-    return;
-  }
-  console.log(`Migration stdout: ${stdout}`);
-});
+ 
 
 sequelize.sync({ force: false }).then(() => {
 app.listen(PORT, () => {
